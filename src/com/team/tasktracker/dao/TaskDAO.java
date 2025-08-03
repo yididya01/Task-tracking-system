9eab581143f41eae733e6d24f57e8c2184e1c967
ragadabdu <ragadabdelwahab81@gmail.com>
2025-08-03 11:21:20 +0300

    Add TaskDAO class with CRUD operations

---
+++ src/com/team/tasktracker/dao/TaskDAO.java
@@ -0,0 +1,37 @@
+package com.team.tasktracker.dao;
+
+import com.team.tasktracker.Task;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TaskDAO {
+
+    // Simulate database with a HashMap
+    private Map<Integer, Task> taskDatabase = new HashMap<>();
+    private int idCounter = 1;
+
+    // Save a new task and assign a unique ID
+    public void save(Task task) {
+        task.setTaskId(idCounter++);
+        taskDatabase.put(task.getTaskId(), task);
+    }
+
+    // Find a task by its ID
+    public Task findById(int id) {
+        return taskDatabase.get(id);
+    }
+
+    // Update an existing task
+    public void update(Task task) {
+        int id = task.getTaskId();
+        if (taskDatabase.containsKey(id)) {
+            taskDatabase.put(id, task);
+        }
+    }
+
+    // Delete a task by its ID
+    public void delete(int id) {
+        taskDatabase.remove(id);
+    }
+}
